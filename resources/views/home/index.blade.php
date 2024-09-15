4528d0295d2b3fc852edb8f4e2c76d810e4165c7
dmisl <rithlesscorp@gmail.com>
2024-09-16 00:23:33 +0200

    zindexes fix

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,12 +14,12 @@
     <div class="menu__parent">
         <div class="menu">
             <div class="card shadow">
-                <a class="menu__element yellow__background goals">Мої цілі</a>
-                <a class="menu__element green__background tasks">Сьогоднішні завдання</a>
-                <a class="menu__element blue__background week">Мій тиждень</a>
-                <a class="menu__element red__background completed">Виконані завдання</a>
-                <a class="menu__element grey__background settings"><p style="z-index: 10;">Налаштування</p></a>
-                <a class="menu__element black__background logout">Вийти з системи</a>
+                <a href="#goals" class="menu__element yellow__background goals">Мої цілі</a>
+                <a href="#tasks" class="menu__element green__background tasks">Сьогоднішні завдання</a>
+                <a href="#week" class="menu__element blue__background week">Мій тиждень</a>
+                <a href="#completed" class="menu__element red__background completed">Виконані завдання</a>
+                <a href="#settings" class="menu__element grey__background settings"><p style="z-index: 10;">Налаштування</p></a>
+                <a href="#logout" class="menu__element black__background logout">Вийти з системи</a>
                 <p class="hint">спробуй</p>
             </div>
         </div>
@@ -83,9 +83,20 @@
             </div>
         </div>
         <div class="moved__container">
-
-            
-
+            <div class="left__part"></div>
+            <div class="right__part">
+                <div class="flex">
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                    <div class="flex__block"></div>
+                </div>
+            </div>
         </div>
     </div>
 
