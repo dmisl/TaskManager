36d720970f452ebc4c50c2984a6d0d4201508090
dmisl <rithlesscorp@gmail.com>
2024-11-29 03:17:08 +0100

    all default localizations translation to ukrainian

--- lang/ua/passwords.php
+++ lang/ua/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Ваш пароль було скинуто.',
+    'sent' => 'Ми надіслали вам на електронну пошту посилання для відновлення паролю.',
+    'throttled' => 'Будь ласка, зачекайте, перш ніж спробувати ще раз.',
+    'token' => 'Цей токен скидання пароля є недійсним.',
+    'user' => "Ми не можемо знайти користувача з такою адресою.",
 
 ];
